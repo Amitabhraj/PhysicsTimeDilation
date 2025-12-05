b5c0b1e487bd07d5e83b0b53145cbb274b58548d
Amitabh Raj <abhiraj1709w@gmail.com>
2025-12-05 20:06:38 +0530

    added some changes

--- physicsTimeDilation/index.py
+++
@@ -1,9 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "physicsTimeDilation.settings")
-
-app = get_wsgi_application()
-
-def handler(request, response):
-    return app(request, response)
